690cc2076eba8a1f015f5ed93b59fc7e4f9ed88f
unknown <varnikka0711@gmail.com>
2026-09-15 01:21:06 +0530

    add: practice week 4

---
+++ week-4/practice/BestTimeStockProfitR4M.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class BestTimeStockProfitR4M {
+    static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        System.out.println(maxProfit(prices));
+    }
+}
